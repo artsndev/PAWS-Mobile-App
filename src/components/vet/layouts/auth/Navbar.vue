cd097c8a76e35da61a524bbc15ae4dd74152b9a7
Al-Fhaigar J. Usman <93189177+artsndev@users.noreply.github.com>
2024-09-11 10:14:38 +0800

    Create Navbar.vue

---
+++ src/components/vet/layouts/auth/Navbar.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <v-app-bar flat active elevation="0" >
+    <template v-slot:prepend>
+      <v-btn icon  @click="goBack">
+          <v-icon size="25">mdi-chevron-left</v-icon>
+        </v-btn>
+    </template>
+    <!-- <v-spacer></v-spacer> -->
+    <v-app-bar-title class="text-center me-4">Login as a User</v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goBack = () => {
+  router.go(-1)
+}
+</script>
