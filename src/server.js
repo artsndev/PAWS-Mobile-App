773cd818f868826d8a5d1d3be72eb14a77b62185
Al-Fhaigar J. Usman <rubickking04@gmail.com>
2024-11-22 13:48:15 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://phplaravel-1334733-4893542.cloudwaysapps.com/api';
+export const BASE_URL = 'https://phplaravel-1364672-5025113.cloudwaysapps.com/api';
 // export const BASE_URL = 'http://127.0.0.1:8000/api';
