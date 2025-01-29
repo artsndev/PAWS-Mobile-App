7f6677eacea3c4cd0396fb79fe014d89b59b895d
Al-Fhaigar J. Usman <rubickking04@gmail.com>
2025-01-29 13:08:13 +0800

    coded by artisandev

--- src/components/vet/Appointment.vue
+++ src/components/vet/Appointment.vue
@@ -100,7 +100,7 @@
                       <v-spacer></v-spacer>
                       <v-btn density="comfortable" @click="markAsDone(item)" text="Accept Appointment" variant="text" color="success">
                       </v-btn>
-                      <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
+                      <v-btn text="Close" @click="isActive.value = false"></v-btn>
                     </v-card-actions>
                   </v-card>
                 </template>
